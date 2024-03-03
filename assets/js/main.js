ae1eb5b4a664df4cf8330ea5835214184b36868a
NishanthSbz <nishanthsbz05@gmail.com>
2024-03-03 23:07:06 +0530

    pre loader problem is fixed by adding a time out to it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,138 @@
-(function($) {
-  
-  "use strict";  
+(function ($) {
 
-  $(window).on('load', function() {
+  "use strict";
 
-  /*Page Loader active
-    ========================================================*/
-    $('#preloader').fadeOut();
+  $(window).on('load', function () {
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 100) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
+    /*Page Loader active
+      ========================================================*/
+    // $('#preloader').fadeOut();
+    setTimeout(function () {
+      $('#preloader').fadeOut();
+    }, 2000); // 10 seconds timeou
+
+    // Sticky Nav
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 100) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+      } else {
+        $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
     });
 
-    
-    
+
+
 
     /* Auto Close Responsive Navbar on Click
     ========================================================*/
     function close_toggle() {
-        if ($(window).width() <= 768) {
-            $('.navbar-collapse a').on('click', function () {
-                $('.navbar-collapse').collapse('hide');
-            });
-        }
-        else {
-            $('.navbar .navbar-inverse a').off('click');
-        }
+      if ($(window).width() <= 768) {
+        $('.navbar-collapse a').on('click', function () {
+          $('.navbar-collapse').collapse('hide');
+        });
+      }
+      else {
+        $('.navbar .navbar-inverse a').off('click');
+      }
     }
     close_toggle();
     $(window).resize(close_toggle);
 
-      /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
+    /* WOW Scroll Spy
+  ========================================================*/
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false
     });
     wow.init();
 
     /* Nivo Lightbox 
     ========================================================*/
     $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
+      effect: 'fadeScale',
+      keyboardNav: true,
+    });
 
     // one page navigation 
     $('.navbar-nav').onePageNav({
-            currentClass: 'active'
-    }); 
+      currentClass: 'active'
+    });
 
     /* Counter
     ========================================================*/
     $('.counterUp').counterUp({
-     delay: 10,
-     time: 1500
+      delay: 10,
+      time: 1500
     });
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
 
-      $('.register-link').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 800);
-        return false;
-      });
+    $('.register-link').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $('.back-to-top').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
 
-  });      
+  });
 
 }(jQuery));
 
- // Set the date to count down to (format: month day, year, hour:minute:second)
- var countDownDate = new Date('Mar 6, 2024 00:00:00').getTime()
-            
- // Update the timer every second
- var x = setInterval(function () {
-   // Get the current date and time
-   var now = new Date().getTime()
- 
-   // Calculate the time remaining
-   var distance = countDownDate - now
- 
-   // Calculate days, hours, minutes, and seconds
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24))
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000)
- 
-   // Display the timer in the #timer div
-   document.getElementById('timer').innerHTML = days + 'days ' + hours + 'hours ' + minutes + 'mins ' + seconds + 'sec '
- 
-   // If the countdown is over, display a message
-   if (distance < 0) {
-     clearInterval(x)
-     document.getElementById('timer').innerHTML = 'EXPIRED'
-   }
- }, 1000)
+// Set the date to count down to (format: month day, year, hour:minute:second)
+var countDownDate = new Date('Mar 6, 2024 00:00:00').getTime()
 
- function scroll() {
+// Update the timer every second
+var x = setInterval(function () {
+  // Get the current date and time
+  var now = new Date().getTime()
+
+  // Calculate the time remaining
+  var distance = countDownDate - now
+
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+  // Display the timer in the #timer div
+  document.getElementById('timer').innerHTML = days + 'days ' + hours + 'hours ' + minutes + 'mins ' + seconds + 'sec '
+
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(x)
+    document.getElementById('timer').innerHTML = 'EXPIRED'
+  }
+}, 1000)
+
+function scroll() {
   var phase = document.getElementsByClassName('tab-content');
   phase.scrollIntoView();
-  
+
 }
 
 // Accordion schedule area
 // const accordionItem = document.querySelectorAll('.accordion-item');
-                             
+
 // const onClickAccordionHeader = e => {
 //   if (e.currentTarget.parentNode.classList.contains('active')) {
 //     e.currentTarget.parentNode.classList.remove("active");
@@ -170,39 +173,39 @@ const init = () => {
 init();
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    navbarToggler.addEventListener('click', function() {
-        const icon = navbarToggler.querySelector('.lni-menu');
-        icon.classList.toggle('open');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  navbarToggler.addEventListener('click', function () {
+    const icon = navbarToggler.querySelector('.lni-menu');
+    icon.classList.toggle('open');
+  });
 
-    // Update the menu symbol when a menu item is clicked
-    const menuItems = document.querySelectorAll('.navbar-nav .nav-link');
-    menuItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            const icon = navbarToggler.querySelector('.lni-menu');
-            icon.classList.remove('open');
-        });
+  // Update the menu symbol when a menu item is clicked
+  const menuItems = document.querySelectorAll('.navbar-nav .nav-link');
+  menuItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      const icon = navbarToggler.querySelector('.lni-menu');
+      icon.classList.remove('open');
     });
+  });
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const script = document.createElement('script');
   script.src = 'https://apply.devfolio.co/v2/sdk.js';
   script.async = true;
   script.defer = true;
   document.body.appendChild(script);
-  
+
   // Optional: If you want to remove the script when the page unloads
-  window.addEventListener('unload', function() {
-      document.body.removeChild(script);
+  window.addEventListener('unload', function () {
+    document.body.removeChild(script);
   });
 });
 
 
-  // Handle scrolling when the user interacts with the scrollbar
+// Handle scrolling when the user interacts with the scrollbar
 // const cardsContainer = document.querySelector('.card');
 
 // cardsContainer.addEventListener('scroll', function() {
@@ -221,5 +224,38 @@ document.addEventListener('DOMContentLoaded', function() {
 //   this.classList.remove('hovered');
 // });
 
+
+
+$(document).ready(function () {
+  var video = document.getElementById('bgvid');
+  var playButton = document.getElementById('playButton');
+
+  // Function to play video when section is in view
+  function playVideo() {
+    if (isElementInViewport($('.ready-to-play'))) {
+      video.play();
+      playButton.style.display = 'none';
+    }
+  }
+
+  // Function to check if an element is in the viewport
+  function isElementInViewport(el) {
+    var rect = el[0].getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // Play video when scrolled into view
+  $(window).on('scroll', function () {
+    playVideo();
+  });
+
+  // Play video when loaded and in view
+  playVideo();
+});
 
 
